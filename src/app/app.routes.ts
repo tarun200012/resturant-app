3e692acf5158f6e3f4871410a59744718439cf9c
nik-1207 <nmknikhilkashyap@gmail.com>
2025-07-14 22:39:00 +0530

    refactor: pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ResturantForm } from './resturant-form/resturant-form';
+import { RestaurantTable } from './restaurant-table/restaurant-table';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RestaurantTable },
+  { path: 'add-restaurant', component: ResturantForm },
+  { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { RestaurantTable } from './restaurant-table/restaurant-table';
-import { ResturantForm } from './resturant-form/resturant-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RestaurantTable, ResturantForm],
+  imports: [RouterOutlet, RestaurantTable],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('resturant-app');
+
+  constructor(private router: Router) {}
 }
